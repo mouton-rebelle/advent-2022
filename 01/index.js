9e396602b11c5f97fcdfb70fee84bf1ac78c144b
Cedric <cf@eg0.me>
2022-12-01 09:16:46 +0100

    end of first day / prettier

--- 01/index.js
+++ 01/index.js
@@ -1,3 +1,13 @@
-const data = require('fs').readFileSync('calories.txt', 'utf8');
-const elves = data.split('\n\n').map(elf => elf.split('\n').reduce((acc, curr) => acc + parseInt(curr), 0));
+const data = require('fs').readFileSync('calories.txt', 'utf8')
+
+const elves = data
+  .split('\n\n')
+  .map(elf => elf.split('\n').reduce((acc, curr) => acc + parseInt(curr), 0))
+  .sort((a, b) => (a < b ? 1 : -1))
+
+const top3 = elves.slice(0, 3)
+
+const caloriesTop3 = top3.reduce((acc, curr) => acc + curr, 0)
+console.log(top3, caloriesTop3)
+
 console.log(Math.max(...elves))
